2d90f9a11f62e89ac33da2801f4c2d45dc446d00
Luck Dragon <dabush1986@gmail.com>
2020-04-22 20:43:01 -0700

    Random opponant and stats generator

--- pokemon_battler.py
+++ pokemon_battler.py
@@ -1,8 +1,11 @@
 import random
+from random import randint
+from pick import pick
 
 class Pokemon:
 
-    def __init__(self, name, level, type, maximum_health, current_health, current_status):
+    def __init__(self, name, level, type, maximum_health, current_health,
+    current_status):
         self.name = name
         self.level = level
         self.type = type
@@ -19,13 +22,32 @@ class Pokemon:
             self.current_health -= self.level
 
     def lose_health(self):
-        print('{name} now has {current_health} health!'.format(name = self.name, current_health = self.current_health))
+        print('{name} now has {current_health} health!'.format(name = self.name,
+        current_health = self.current_health))
     def gain_health(self):
-        print('{name} now has {current_health} health!'.format(name = self.name, current_health = self.current_health))
+        print('{name} now has {current_health} health!'.format(name = self.name,
+        current_health = self.current_health))
     def knock_out(self):
         print('{name} has been knocked out!'.format(name = self.name))
     def revive(self):
         print('{name} has been revived!'.format(name = self.name))
+
+
+class Trainer:
+    def __init__(self, name, fighting_pokemon, potions, pokemon_inventory):
+        self.name = name
+        self.fighting_pokemon = fighting_pokemon
+        self.potions = potions
+        self.pokemon_inventory = pokemon_inventory
+
+    def __repr__(self):
+        return """Opponent: {name}
+Pokemon: {active_pokemon}
+Items Available: {potions} Potions
+Available Pokemon: {pokemon_inventory}\n""".format(name=self.name,
+active_pokemon=self.fighting_pokemon, potions
+        =self.potions, pokemon_inventory=self.pokemon_inventory)
+
 
 #different pokemon
 charmander = Pokemon('Charmander', 10, 'Fire', 100, 100, 'Alert')
@@ -34,18 +56,20 @@ pikachu = Pokemon('Pikachu', 10, 'Electric', 100, 100, 'Alert')
 bulbasaur = Pokemon('Bulbasaur', 10, 'Grass', 100, 100, 'Alert')
 psyduck = Pokemon('Psyduck', 10, 'Pychic', 100, 100, 'Alert')
 beedrill = Pokemon('Beedrill', 10, 'Bug', 100, 100, 'Alert')
-
-class Trainer:
-    def __init__(self, name, pokemon_list, potion, active_pokemon):
-        self.name = name
-        self.pokemon_list = pokemon_list
-        self.potion = potion
-        self.active_pokemon = active_pokemon
-
-    def repr(self):
-        self.name = random.choice(trainer_names)
-        return "{name} will be the first challenger".format(name=self.name)
+pokemon_list = [charmander, squirtle, pikachu, bulbasaur, psyduck, beedrill]
 
 trainer_names = ['Brock', 'Misty', 'Ash', 'Gary', 'Jessie', 'James']
-trainer_one = Trainer()
-print(trainer_one)
+
+#Random opponant generator
+opponent_team = []
+opponent_pokemon = random.choice(pokemon_list).name
+teams = random.sample(pokemon_list, random.randint(1,6))
+for team in teams:
+    if team.name == opponent_pokemon: #player's pokemon not in unused inventory
+        continue
+    else:
+        opponent_team.append(team.name)
+trainer_two = Trainer(random.choice(trainer_names),
+opponent_pokemon, randint(0,10), player_team)
+
+print(trainer_two)
